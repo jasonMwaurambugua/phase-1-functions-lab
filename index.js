7eb6d1758ff0b5b9b54bc5d781404448a18058c0
jason mwaura mbugua <jason.mwaura@student.moringaschool.com>
2022-08-24 00:45:54 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+//phase-1functions-lab
+//jason
 // Code your solution in this file!
+
+function distanceFromHqInBlocks($43);($1)=>{
+    
+  };
+
+  function distanceFromHqInFeet($50){
+    distanceFromHqInBlocks($8);
+   
+  }
+
+  function distanceTravelledInFeet($0, $264) {
+    
+  }
+
+  function calculatesFarePrice($2001, $2500) {
+    
+  }
